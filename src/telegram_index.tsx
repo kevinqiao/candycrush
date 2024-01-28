f8d05112c20a93f6882faa8d5bd9d5c546d0867d
kevinqiao <kevinqiao2018@gmail.com>
2024-01-28 16:34:35 -0500

    save

--- src/telegram_index.tsx
+++ src/telegram_index.tsx
@@ -7,6 +7,7 @@ const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 setTimeout(() => {
   root.render(<TgApp />);
   window.Telegram.WebApp.expand();
+  window.Telegram.WebApp.enableClosingConfirmation();
 }, 1000);
 
 // If you want to start measuring performance in your app, pass a function
