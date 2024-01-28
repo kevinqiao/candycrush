941acc67e374273dbc88acd7006548315f954a8b
kevinqiao <kevinqiao2018@gmail.com>
2024-01-28 13:18:54 -0500

    save

--- src/TgApp.tsx
+++ src/TgApp.tsx
@@ -15,7 +15,7 @@ gsap.registerPlugin(MotionPathPlugin);
 const convex = new ConvexReactClient("https://dazzling-setter-839.convex.cloud");
 function TgApp() {
   window.Telegram.WebApp.expand();
-  setTimeout(() => window.Telegram.WebApp.expand(), 500);
+  setTimeout(() => window.Telegram.WebApp.expand(), 1500);
   const FlattenedProviderTree = (providers: any): any => {
     if (providers?.length === 1) {
       return providers[0][0];
