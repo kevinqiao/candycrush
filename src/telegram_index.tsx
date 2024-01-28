9e1f5cd00e6bb23d41fd211002ca637722a28da4
kevinqiao <kevinqiao2018@gmail.com>
2024-01-28 15:21:17 -0500

    save

--- src/telegram_index.tsx
+++ src/telegram_index.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(<TgApp />);
+setTimeout(() => root.render(<TgApp />), 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
