f5a40c90b8d3641501fa1186fc68e945755a8eba
kevinqiao <kevinqiao2018@gmail.com>
2024-02-01 12:57:55 -0500

    save

--- src/component/animation/BattleAnimateHandler.ts
+++ src/component/animation/BattleAnimateHandler.ts
@@ -1,6 +1,7 @@
 import { gsap } from "gsap";
 import { useCallback, useEffect } from "react";
 import { useBattleManager } from "service/BattleManager";
+import { useUserManager } from "service/UserManager";
 import * as gameEngine from "../../service/GameEngine";
 import { ANIMATE_EVENT_TYPE, ANIMATE_NAME } from "./AnimateConstants";
 import { IAnimateHandleContext } from "./AnimateManager";
@@ -9,6 +10,7 @@ import useGameReady from "./game/GameReady";
 
 export const useBattleAnimateHandler = (props: IAnimateHandleContext) => {
     const { animateEvent } = props;
+    const { user } = useUserManager();
     const { battle } = useBattleManager();
     const { initGame } = useGameReady();
     const { initConsole } = useBattleBoard();
@@ -20,6 +22,7 @@ export const useBattleAnimateHandler = (props: IAnimateHandleContext) => {
                 timeline.kill();
             }
         });
+        console.log(user)
         const bl = gsap.timeline();
         timeline.add(bl, ">+=1.0")
         battle.games.forEach((g) => {
